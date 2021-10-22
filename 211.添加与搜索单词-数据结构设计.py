0b39fcbaf5761d264c8711864e2bc50727c35944
Thomas Young <sealinglip@gmail.com>
2021-10-22 12:11:41 +0800

    211022打卡

---
+++ 211.添加与搜索单词-数据结构设计.py
@@ -0,0 +1,103 @@
+#
+# @lc app=leetcode.cn id=211 lang=python3
+#
+# [211] 添加与搜索单词 - 数据结构设计
+#
+# 请你设计一个数据结构，支持 添加新单词 和 查找字符串是否与任何先前添加的字符串匹配 。
+
+# 实现词典类 WordDictionary ：
+
+# WordDictionary() 初始化词典对象
+# void addWord(word) 将 word 添加到数据结构中，之后可以对它进行匹配
+# bool search(word) 如果数据结构中存在字符串与 word 匹配，则返回 true ；否则，返回  false 。word 中可能包含一些 '.' ，每个 . 都可以表示任何一个字母。
+
+
+# 示例：
+
+# 输入：
+# ["WordDictionary", "addWord", "addWord", "addWord",
+#     "search", "search", "search", "search"]
+# [[], ["bad"], ["dad"], ["mad"], ["pad"], ["bad"], [".ad"], ["b.."]]
+# 输出：
+# [null, null, null, null, false, true, true, true]
+
+# 解释：
+# WordDictionary wordDictionary = new WordDictionary()
+# wordDictionary.addWord("bad")
+# wordDictionary.addWord("dad")
+# wordDictionary.addWord("mad")
+# wordDictionary.search("pad")
+# // return False
+# wordDictionary.search("bad")
+# // return True
+# wordDictionary.search(".ad")
+# // return True
+# wordDictionary.search("b..")
+# // return True
+
+
+# 提示：
+# 1 <= word.length <= 500
+# addWord 中的 word 由小写英文字母组成
+# search 中的 word 由 '.' 或小写英文字母组成
+# 最多调用 50000 次 addWord 和 search
+
+# @lc code=start
+class WordDictionary:
+
+    def __init__(self):
+        self.charArr = [None] * 26
+        self.isEnd = False
+
+    def addWord(self, word: str) -> None:
+        node = self
+        for c in word:
+            idx = ord(c) - ord('a')
+            if not node.charArr[idx]:
+                node.charArr[idx] = WordDictionary()
+            node = node.charArr[idx]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self
+        for i, c in enumerate(word):
+            if c == '.':
+                rest = word[i+1:]
+                for child in node.charArr:
+                    if child and child.search(rest):
+                        return True
+                return False
+            else:
+                idx = ord(c) - ord('a')
+                if node.charArr[idx]:
+                    node = node.charArr[idx]
+                else:
+                    node = None
+                    break
+        return (node is not None) and node.isEnd
+
+
+        # Your WordDictionary object will be instantiated and called as such:
+        # obj = WordDictionary()
+        # obj.addWord(word)
+        # param_2 = obj.search(word)
+        # @lc code=end
+if __name__ == "__main__":
+    # wordDictionary = WordDictionary()
+    # wordDictionary.addWord("bad")
+    # wordDictionary.addWord("dad")
+    # wordDictionary.addWord("mad")
+    # print(wordDictionary.search("pad"))  # // return False
+    # print(wordDictionary.search("bad"))  # // return True
+    # print(wordDictionary.search(".ad"))  # // return True
+    # print(wordDictionary.search("b.."))  # // return True
+
+    wordDictionary = WordDictionary()
+    wordDictionary.addWord("a")
+    wordDictionary.addWord("a")
+    # print(wordDictionary.search("."))  # // return True
+    # print(wordDictionary.search("a"))  # // return True
+    # print(wordDictionary.search("aa"))  # // return False
+    # print(wordDictionary.search("a"))  # // return True
+    print(wordDictionary.search("a."))  # // return False
+    print(wordDictionary.search(".a"))  # // return False
---
+++ 229.求众数-ii.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode.cn id=229 lang=python3
+#
+# [229] 求众数 II
+#
+# 给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+
+
+# 示例 1：
+# 输入：[3, 2, 3]
+# 输出：[3]
+
+# 示例 2：
+# 输入：nums = [1]
+# 输出：[1]
+
+# 示例 3：
+# 输入：[1, 1, 1, 3, 3, 2, 2, 2]
+# 输出：[1, 2]
+
+
+# 提示：
+# 1 <= nums.length <= 5 * 104
+# -109 <= nums[i] <= 109
+
+
+# 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1)的算法解决此问题。
+
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        ele1, ele2 = 0, 0
+        cnt1, cnt2 = 0, 0
+        N = len(nums)
+
+        for num in nums:
+            if cnt1 > 0 and num == ele1:
+                cnt1 += 1
+            elif cnt2 > 0 and num == ele2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                cnt1 += 1
+                ele1 = num
+            elif cnt2 == 0:
+                cnt2 += 1
+                ele2 = num
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+
+        cnt1, cnt2 = 0, 0
+        res = []
+        for num in nums:
+            if num == ele1:
+                cnt1 += 1
+            elif num == ele2:
+                cnt2 += 1
+
+        if cnt1 > (N // 3):
+            res.append(ele1)
+        if cnt2 > (N // 3):
+            res.append(ele2)
+        return res
+
+
+# @lc code=end
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.majorityElement([3, 2, 3]))
+    print(solution.majorityElement([1]))
+    print(solution.majorityElement([1, 1, 1, 3, 3, 2, 2, 2]))
