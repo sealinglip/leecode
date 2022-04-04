dda42673448472c62f5d25a659b016c1a9d856d4
Thomas Young <sealinglip@gmail.com>
2022-04-04 13:07:52 +0800

    220404打卡

---
+++ 307.区域和检索-数组可修改.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode.cn id=307 lang=python3
+#
+# [307] 区域和检索 - 数组可修改
+#
+# 给你一个数组 nums ，请你完成两类查询。
+
+# 其中一类查询要求 更新 数组 nums 下标对应的值
+# 另一类查询要求返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 ，其中 left <= right
+# 实现 NumArray 类：
+
+# NumArray(int[] nums) 用整数数组 nums 初始化对象
+# void update(int index, int val) 将 nums[index] 的值 更新 为 val
+# int sumRange(int left, int right) 返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 （即，nums[left] + nums[left + 1], ..., nums[right]）
+
+
+# 示例 1：
+# 输入：
+# ["NumArray", "sumRange", "update", "sumRange"]
+# [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+# 输出：
+# [null, 9, null, 8]
+# 解释：
+# NumArray numArray = new NumArray([1, 3, 5])
+# numArray.sumRange(0, 2)
+# // 返回 1 + 3 + 5 = 9
+# numArray.update(1, 2)
+# // nums = [1, 2, 5]
+# numArray.sumRange(0, 2)
+# // 返回 1 + 2 + 5 = 8
+
+
+# 提示：
+# 1 <= nums.length <= 3 * 10^4
+# -100 <= nums[i] <= 100
+# 0 <= index < nums.length
+# -100 <= val <= 100
+# 0 <= left <= right < nums.length
+# 调用 update 和 sumRange 方法次数不大于 3 * 10^4
+
+
+from typing import List
+# @lc code=start
+SEC_SIZE = 50  # 分区大小
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.secSum = [0] * ((len(nums) + SEC_SIZE - 1) // SEC_SIZE)  # 分区求和
+        for i, num in enumerate(nums):
+            j = i // SEC_SIZE
+            self.secSum[j] += num
+
+    def update(self, index: int, val: int) -> None:
+        delta = val - self.nums[index]
+        self.nums[index] = val
+        self.secSum[index // SEC_SIZE] += delta
+
+    def sumRange(self, left: int, right: int) -> int:
+        i = left // SEC_SIZE
+        j = right // SEC_SIZE
+        # 和由三部分组成
+        if i == j:
+            return sum(self.nums[left:right+1], 0)
+        else:
+            return sum(self.nums[left:(i+1)*SEC_SIZE]) + sum(self.secSum[i+1:j]) + sum(self.nums[j*SEC_SIZE:right+1])
+
+        # Your NumArray object will be instantiated and called as such:
+        # obj = NumArray(nums)
+        # obj.update(index,val)
+        # param_2 = obj.sumRange(left,right)
+        # @lc code=end
---
+++ 420.强密码检验器.py
@@ -0,0 +1,120 @@
+#
+# @lc app=leetcode.cn id=420 lang=python3
+#
+# [420] 强密码检验器
+#
+# 如果一个密码满足下述所有条件，则认为这个密码是强密码：
+# 由至少 6 个，至多 20 个字符组成。
+# 至少包含 一个小写 字母，一个大写 字母，和 一个数字 。
+# 同一字符 不能 连续出现三次(比如 "...aaa..." 是不允许的, 但是 "...aa...a..." 如果满足其他条件也可以算是强密码)。
+# 给你一个字符串 password ，返回 将 password 修改到满足强密码条件需要的最少修改步数。如果 password 已经是强密码，则返回 0 。
+
+# 在一步修改操作中，你可以：
+# 插入一个字符到 password ，
+# 从 password 中删除一个字符，或
+# 用另一个字符来替换 password 中的某个字符。
+
+
+# 示例 1：
+# 输入：password = "a"
+# 输出：5
+
+# 示例 2：
+# 输入：password = "aA1"
+# 输出：3
+
+# 示例 3：
+# 输入：password = "1337C0d3"
+# 输出：0
+
+
+# 提示：
+# 1 <= password.length <= 50
+# password 由字母、数字、点 '.' 或者感叹号 '!'
+
+
+# @lc code=start
+
+class Solution:
+    def strongPasswordChecker(self, password: str) -> int:
+        n = len(password)
+        if n < 5:
+            # 长度为5以下（不含）的密码，修改成强密码，其次数就是长度差值，通过插入字符，一定能同时满足密码长度、字符多样性和连续重复字符长度限制的条件
+            return 6 - n
+        else:
+            lower = upper = digit = 0
+            for c in password:
+                if c.isupper():
+                    upper = 1
+                elif c.islower():
+                    lower = 1
+                elif c.isdigit():
+                    digit = 1
+
+            categories = lower + upper + digit
+            if n < 6:
+                return max(6 - n, 3 - categories)
+            elif n < 21:
+                replace = cnt = 0
+                cur = ""
+
+                for c in password:
+                    if c == cur:
+                        cnt += 1
+                    else:
+                        replace += cnt // 3
+                        cnt = 1
+                        cur = c
+
+                replace += cnt // 3
+                return max(replace, 3 - categories)
+            else:
+                # 替换次数和删除次数
+                replace, remove = 0, n - 20
+                # k mod 3 = 1 的组数，即删除 2 个字符可以减少 1 次替换操作
+                rm2 = cnt = 0
+                cur = "#"
+
+                for ch in password:
+                    if ch == cur:
+                        cnt += 1
+                    else:
+                        if remove > 0 and cnt >= 3:
+                            if cnt % 3 == 0:
+                                # 如果是 k % 3 = 0 的组，那么优先删除 1 个字符，减少 1 次替换操作
+                                remove -= 1
+                                replace -= 1
+                            elif cnt % 3 == 1:
+                                # 如果是 k % 3 = 1 的组，那么存下来备用
+                                rm2 += 1
+                            # k % 3 = 2 的组无需显式考虑
+                        replace += cnt // 3
+                        cnt = 1
+                        cur = ch
+
+                if remove > 0 and cnt >= 3:
+                    if cnt % 3 == 0:
+                        remove -= 1
+                        replace -= 1
+                    elif cnt % 3 == 1:
+                        rm2 += 1
+
+                replace += cnt // 3
+
+                # 使用 k % 3 = 1 的组的数量，由剩余的替换次数、组数和剩余的删除次数共同决定
+                use2 = min(replace, rm2, remove // 2)
+                replace -= use2
+                remove -= use2 * 2
+                # 由于每有一次替换次数就一定有 3 个连续相同的字符（k / 3 决定），因此这里可以直接计算出使用 k % 3 = 2 的组的数量
+                use3 = min(replace, remove // 3)
+                replace -= use3
+                remove -= use3 * 3
+                return (n - 20) + max(replace, 3 - categories)
+# @lc code=end
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.strongPasswordChecker("a"))  # 5
+    print(solution.strongPasswordChecker("aA1"))  # 3
+    print(solution.strongPasswordChecker("1337C0d3"))  # 0
